92297129777382ce4991beea4ced74334fa5e140
Tobias Trozowski <tobias.trozowski@sirrus-systems.de>
2015-02-24 13:12:54 +0100

    added missing license and copyright

--- tests/MangentoTest/Framework/Stdlib/ArrayUtilsTest.php
+++ tests/MangentoTest/Framework/Stdlib/ArrayUtilsTest.php
@@ -1,4 +1,8 @@
 <?php
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
 namespace MagentoTest\Framework\Stdlib;
 
 use Magento\Framework\Stdlib\ArrayUtils;
